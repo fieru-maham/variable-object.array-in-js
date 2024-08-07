958ddd7eb180ba7212b46d4c3e1cc670cac6d39a
fieru-maham <aamirbasma@gmail.com>
2024-08-07 19:25:51 +0500

    abc

---
+++ js/main.js
@@ -0,0 +1,43 @@
+
+// Variable
+
+console.log("Variable");
+
+let stdA = "Ahmed";
+
+let stdB = "Bilal"; 
+
+console.log(stdA);
+
+// object
+
+console.log("object");
+
+let stdC ={
+    firstName : "Ahmed",
+    lastName : "Khan",
+    age : 18,
+}
+let stdD ={
+    firstName : "Bilal",
+    lastName : "Iqbal",
+    age : 16,
+}
+let stdE ={
+    firstName : "Abdullah",
+    lastName : "Javed",
+    age : 17,
+}
+
+console.log(stdE);
+
+// array
+
+console.log("array");
+
+let names = ["Ahmed", "Billal", "Abdullah", "Usman", "Ali", "Musa"];
+
+let namesOfStudents = [names[1], names[5]];
+
+console.log(namesOfStudents);
+
